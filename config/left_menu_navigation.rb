ed2ed6199b8347b0a46c3f8a3db587d0ed33d2ac
zzorin <ilya-zorin@yandex.ru>
2020-03-13 15:45:34 +0700

    added left_menu navigation styles

--- config/left_menu_navigation.rb
+++ config/left_menu_navigation.rb
@@ -2,7 +2,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
 
   navigation.items do |primary|
-    primary.item :criterions, 'Конкурс', contest_path(@contest)
+    primary.item :contest, 'Конкурс', contest_path(@contest)
     primary.item :criterions, 'Критерии отбора', contest_criterions_path(@contest)
     primary.item :nominations, 'Номинации', contest_nominations_path(@contest)
   end
