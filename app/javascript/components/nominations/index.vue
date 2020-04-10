a259948671fc7b618e4f5c88fe601d1f1399de40
Mochalova Victoria <mva@openteam.ru>
2020-04-10 13:38:36 +0700

    styled nominations

--- app/javascript/components/nominations/index.vue
+++ app/javascript/components/nominations/index.vue
@@ -1,44 +1,56 @@
 <template>
   <div>
-    <div v-if='isCurrentPage("nominations")'>
-      <div class="mt-4 mb-1"><strong>Название номинации</strong></div>
-      <div class="d-flex">
-        <input type="text" class="form-control input-with-button" v-model='newNomination.code'/>
-        <input type="text" class="form-control input-with-button" v-model='newNomination.title'/>
-        <span class='btn btn-blue' @click='selfCreateNomination' :class="{'disabled': (!newNomination.title || !newNomination.code)}">
-            Добавить номинацию
-        </span>
+    <div v-if='isCurrentPage("nominations")' class="mt-4">
+      <div class="row">
+        <div class="col-lg-3">
+          <label><strong>Код номинации</strong></label>
+          <input type="text" class="form-control" v-model='newNomination.code'/>
+        </div>
+        <div class="col-lg-6">
+          <label><strong>Название номинации</strong></label>
+          <input type="text" class="form-control" v-model='newNomination.title'/>
+        </div>
+        <div class="col-lg-3">
+          <span class='btn btn-blue' @click='selfCreateNomination' :class="{'disabled': (!newNomination.title || !newNomination.code)}">
+              Добавить номинацию
+          </span>
+        </div>
       </div>
 
       <div class="nominations-list">
         <div v-for='nomination in nominations'>
-          <div v-if='editableNomination.id == nomination.id'>
-            <input type="text" class="form-control" v-model='editableNomination.code'/>
-            <input type="text" class="form-control" v-model='editableNomination.title'/>
-            <span class='btn btn-blue mt-3' @click='selfUpdateNomination(editableNomination)'>
-              Сохранить
+          <div  v-if='!(editableNomination.id == nomination.id)'>
+            <span>
+              {{ nomination.code }}
+              {{ nomination.title }}
             </span>
-            <span class='btn btn-blue mt-3' @click='clearEditableNomination'>
-              Отменить
+            <span class='cursor-pointer' @click='setEditableNomination(nomination)'>
+              <svg class="bi bi-pencil ml-2" width="16px" height="16px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+              </svg>
+            </span>
+            <span class='cursor-pointer' @click='selfDeleteNomination(nomination)'>
+              <svg class="bi bi-x" width="26px" height="26px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd"/>
+              </svg>
             </span>
           </div>
 
-          <span v-if='!(editableNomination.id == nomination.id)'>
-            {{ nomination.code }}
-            {{ nomination.title }}
-          </span>
-          <span class='cursor-pointer' @click='setEditableNomination(nomination)'>
-            <svg class="bi bi-pencil" width="16px" height="16px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
-              <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
-            </svg>
-          </span>
-          <span class='cursor-pointer' @click='selfDeleteNomination(nomination)'>
-            <svg class="bi bi-x" width="26px" height="26px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-              <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd"/>
-            </svg>
-          </span>
+          <div v-if='editableNomination.id == nomination.id'>
+            <div class="row">
+              <div class="col-lg-3"><input type="text" class="form-control" v-model='editableNomination.code'/></div>
+              <div class="col-lg-6"><input type="text" class="form-control" v-model='editableNomination.title'/></div>
+              <span class='btn btn-blue mr-2 ml-3' @click='selfUpdateNomination(editableNomination)'>
+                Сохранить
+              </span>
+              <span class='btn btn-blue' @click='clearEditableNomination'>
+                Отменить
+              </span>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
