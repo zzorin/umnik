b2948e4e3b1f1a652091cd799bd700434ae22325
Mochalova Victoria <mva@openteam.ru>
2020-04-10 12:59:35 +0700

    added some styles to criterions, corrected layout

--- app/javascript/components/criterions/index.vue
+++ app/javascript/components/criterions/index.vue
@@ -11,31 +11,35 @@
 
       <div class="criterions-list">
         <div v-for='criterion in criterions'>
-          <div v-if='editableCriterion.id == criterion.id'>
-            <input type="text" class="form-control" v-model='editableCriterion.title'/>
-            <span class='btn btn-blue mt-3' @click='selfUpdateCriterion(editableCriterion)'>
+
+          <div  v-if='!(editableCriterion.id == criterion.id)' class="mb-2">
+            <span>
+              {{ criterion.title }}
+            </span>
+            <span class='cursor-pointer ml-2' @click='setEditableCriterion(criterion)'>
+              <svg class="bi bi-pencil" width="16px" height="16px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+              </svg>
+            </span>
+            <span class='cursor-pointer' @click='selfDeleteCriterion(criterion)'>
+              <svg class="bi bi-x" width="26px" height="26px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd"/>
+              </svg>
+            </span>
+          </div>
+
+          <div v-if='editableCriterion.id == criterion.id' class="d-flex mb-2">
+            <input type="text" class="form-control input-with-button" v-model='editableCriterion.title'/>
+            <span class='btn btn-blue mr-2' @click='selfUpdateCriterion(editableCriterion)'>
               Сохранить
             </span>
-            <span class='btn btn-blue mt-3' @click='clearEditableCriterion'>
+            <span class='btn btn-blue' @click='clearEditableCriterion'>
               Отменить
             </span>
           </div>
 
-          <span v-if='!(editableCriterion.id == criterion.id)'>
-            {{ criterion.title }}
-          </span>
-          <span class='cursor-pointer' @click='setEditableCriterion(criterion)'>
-            <svg class="bi bi-pencil" width="16px" height="16px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
-              <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
-            </svg>
-          </span>
-          <span class='cursor-pointer' @click='selfDeleteCriterion(criterion)'>
-            <svg class="bi bi-x" width="26px" height="26px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-              <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd"/>
-            </svg>
-          </span>
         </div>
       </div>
     </div>
