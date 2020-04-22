cb5171e84881ceeabc49acd41f906090c133645f
Mochalova Victoria <mva@openteam.ru>
2020-04-22 12:55:20 +0700

    corrected contests layout

--- app/javascript/components/contests/index.vue
+++ app/javascript/components/contests/index.vue
@@ -3,28 +3,30 @@
     <div v-if='isCurrentPage("contests")' class="contests-list margin-bottom-40">
       <h1>Список конкурсов</h1>
       <template v-if='access_rules.contests.new'>
-        <div class="mb-1"><strong>Название конкурса</strong></div>
+        <div class="mb-1"><strong>Название конкурса</strong><span class="text-danger">*</span></div>
         <div class="d-flex">
           <input type="text"
                  class="input-with-button form-control"
                  v-model='newContest.title'/>
         </div>
-        <div class="mb-1"><strong>Период проведения</strong></div>
+        <div class="mb-1 mt-3"><strong>Период проведения</strong><span class="text-danger">*</span></div>
         <div class="d-flex">
           <datepicker v-model='newContest.starts_on'
+            class='mr-3'
             :value='newContest.starts_on'
             :monday-first='true'
             placeholder='Дата начала'
             input-class='form-control background-color-white-important'
             :language='ru' />
           <datepicker v-model='newContest.ends_on'
+            class='mr-3'
             :value='newContest.ends_on'
             :monday-first='true'
             placeholder='Дата окончания'
             input-class='form-control background-color-white-important'
             :language='ru' />
         </div>
-        <span class='btn btn-blue margin-top-10'
+        <span class='btn btn-blue mt-3'
               @click='selfCreateContest'
               :class="{'disabled': !newContest.title}">
           Добавить конкурс
@@ -32,68 +34,78 @@
       </template>
 
       <div class="contests-list-table">
-        <table>
-          <tr v-for='contest in contests'>
-            <template  v-if='!(editableContest.id == contest.id)'>
-              <td>
-                <router-link :to="{name: 'experts', params: { id: contest.id }}">
-                  {{ contest.title }}
-                </router-link>
-              </td>
-              <td>
-                {{ contest.starts_on_format }}
-              </td>
-              <td>
-                {{ contest.ends_on_format }}
-              </td>
-              <td>
-                <span class='cursor-pointer ml-2' @click='setEditableContest(contest)'>
-                  <svg class="bi bi-pencil" width="16px" height="16px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
-                    <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
-                  </svg>
-                </span>
-                <span v-if='access_rules.contests.destroy'
-                      @click='selfDeleteContest(contest)'
-                      class='cursor-pointer'>
-                  <svg class="bi bi-x" width="26px" height="26px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-                    <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd"/>
-                  </svg>
-                </span>
-              </td>
-            </template>
-            <template  v-if='(editableContest.id == contest.id)'>
-              <td>
-                <input type="text" v-model='editableContest.title'/>
-              </td>
-              <td>
-                <datepicker v-model='editableContest.starts_on'
-                  :value='editableContest.starts_on'
-                  :monday-first='true'
-                  placeholder='Дата начала'
-                  input-class='form-control background-color-white-important'
-                  :language='ru' />
-              </td>
-              <td>
-                <datepicker v-model='editableContest.ends_on'
-                  :value='editableContest.ends_on'
-                  :monday-first='true'
-                  placeholder='Дата начала'
-                  input-class='form-control background-color-white-important'
-                  :language='ru' />
-              </td>
-              <td>
-                <span class='btn btn-green mr-2 ml-3'
-                      @click='selfUpdateContest(editableContest)'>
-                  Сохранить
-                </span>
-                <span class='btn btn-light-blue' @click='clearEditableContest'>
-                  Отменить
-                </span>
-              </td>
-            </template>
-          </tr>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Название конкурса</th>
+              <th>Дата начала</th>
+              <th>Дата окончания</th>
+              <th>Действия</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for='contest in contests'>
+              <template  v-if='!(editableContest.id == contest.id)'>
+                <td>
+                  <router-link :to="{name: 'experts', params: { id: contest.id }}">
+                    {{ contest.title }}
+                  </router-link>
+                </td>
+                <td>
+                  {{ contest.starts_on_format }}
+                </td>
+                <td>
+                  {{ contest.ends_on_format }}
+                </td>
+                <td>
+                  <span class='cursor-pointer' @click='setEditableContest(contest)'>
+                    <svg class="bi bi-pencil" width="16px" height="16px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+                      <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+                    </svg>
+                  </span>
+                  <span v-if='access_rules.contests.destroy'
+                        @click='selfDeleteContest(contest)'
+                        class='cursor-pointer'>
+                    <svg class="bi bi-x" width="26px" height="26px" viewBox="0 0 16 16" fill="#0390C8" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd"/>
+                      <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd"/>
+                    </svg>
+                  </span>
+                </td>
+              </template>
+              <template  v-if='(editableContest.id == contest.id)'>
+                <td>
+                  <input type="text" v-model='editableContest.title'/>
+                </td>
+                <td class="contests-table">
+                  <datepicker v-model='editableContest.starts_on'
+                    :value='editableContest.starts_on'
+                    :monday-first='true'
+                    placeholder='Дата начала'
+                    input-class='form-control background-color-white-important'
+                    :language='ru' />
+                </td>
+                <td>
+                  <datepicker v-model='editableContest.ends_on'
+                    :value='editableContest.ends_on'
+                    :monday-first='true'
+                    placeholder='Дата окончания'
+                    input-class='form-control background-color-white-important'
+                    :language='ru' />
+                </td>
+                <td>
+                  <span class='btn btn-green mr-2 ml-3'
+                        @click='selfUpdateContest(editableContest)'>
+                    Сохранить
+                  </span>
+                  <span class='btn btn-light-blue' @click='clearEditableContest'>
+                    Отменить
+                  </span>
+                </td>
+              </template>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
