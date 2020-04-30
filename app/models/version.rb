a1f44ec521b933daf90284628ee0e9d022c0d019
Dcreo <nazarovbusiness@gmail.com>
2020-04-30 12:03:15 +0700

    cleaned up history humanizer class and added empty history state text

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,4 +2,8 @@ class Version < PaperTrail::Version
   def user
     User.find_by(id: whodunnit)
   end
+
+  def self.available_events
+    Version.pluck(:event).uniq.compact
+  end
 end
--- lib/version_normalizer.rb
+++ lib/version_normalizer.rb
@@ -43,65 +43,14 @@ class VersionNormalizer
   def format_changed_fields(key, array, divider)
     array.map do |value|
       value = value.downcase if value.is_a?(String)
+      value = 'не указано' if value.nil? && event_update?
 
       if [true, false].include?(value)
         value ? 'Да' : 'Нет'
-      elsif type == 'Vacancy'
-        value_for_vacancy(key, value)
-      elsif type == 'FileCopy' && value.present?
-        value_label_for_file(key, value)
-      elsif type == 'Document' && value.present?
-        value_for_document(key, value)
-      elsif type == 'Organization'
-        value_for_organization(key, value)
       else
         value
       end
     end.compact.join(divider)
-  end
-
-  def value_for_organization(key, value)
-    case key.to_sym
-    when :state
-      Organization.available_states_with_human_name[value.to_sym] rescue 'Не указана'
-    else
-      value
-    end
-  end
-
-  def value_for_vacancy(key, value)
-    case key.to_sym
-    when :state
-      Vacancy.available_states_with_human_name[value.to_sym] rescue 'Не указана'
-    when :salary_currency, :category, :schedule, :education
-      if value
-        I18n.t("enumerize.vacancy.#{key.to_s}.#{value}").to_s
-      else
-        'Нет значения'
-      end
-    when :created_on
-      I18n.l(value, format: :long) rescue 'Не указана'
-    else
-      value
-    end
-  end
-
-  def value_for_document(key, value)
-    case key.to_sym
-    when :kind
-      I18n.t("enumerize.document.kind.#{value}")
-    when :approval_date
-      I18n.l(value, format: :long)
-    else
-      value
-    end
-  end
-
-  def value_label_for_file(key, value)
-    case key.to_sym
-    when :document_type
-      I18n.t("enumerize.file_copy.document_type.#{value}")
-    end
   end
 
   def set_event_title
@@ -116,5 +65,11 @@ class VersionNormalizer
     end
 
     I18n.t("paper_trail.#{version.item_type.underscore.downcase}.#{gender}.#{version.event}")
+  end
+
+  Version.available_events.each do |event_name|
+    define_method "event_#{event_name}?" do
+      version.event == event_name
+    end
   end
 end
