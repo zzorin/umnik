59f7e346ceb7714080b2d6d88dcd66ccec85b62e
Evgeniy Lapin <lev@openteam.ru>
2020-05-07 04:13:19 +0700

    updated rails

---
+++ config/initializers/non_digest_assets.rb
@@ -0,0 +1,16 @@
+NonStupidDigestAssets.whitelist = [
+  'application.css',
+  'application.js',
+  'mail.png',
+  'placemark.png',
+  'player.swf',
+  'tusur_logotype.png',
+  'video-640x360.txt',
+  /FontAwesome*/,
+  /OfficinaSerifBoldC*/,
+  /OfficinaSerifBookSCC*/,
+  /PTSans*/,
+  /ckeditor\/.*/,
+  /bootstrap\/glyphicons-halflings-regular*/,
+  /fontawesome-webfont*/,
+]
